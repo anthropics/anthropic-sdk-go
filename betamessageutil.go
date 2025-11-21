45c2079520381635389fd2b9b44e501ee3fba0cd
Jacob Zimmerman <jacob@stainless.com>
2025-11-21 10:37:50 -0500

    add complete ToParam methods
    add more to param converters

--- betamessageutil.go
+++ betamessageutil.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/anthropics/anthropic-sdk-go/internal/paramutil"
+	"github.com/anthropics/anthropic-sdk-go/packages/param"
 )
 
 // Accumulate builds up the Message incrementally from a MessageStreamEvent. The Message then can be used as
@@ -100,6 +101,33 @@ func (r BetaContentBlockUnion) ToParam() BetaContentBlockParamUnion {
 	case BetaRedactedThinkingBlock:
 		p := variant.ToParam()
 		return BetaContentBlockParamUnion{OfRedactedThinking: &p}
+	case BetaWebSearchToolResultBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfWebSearchToolResult: &p}
+	case BetaBashCodeExecutionToolResultBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfBashCodeExecutionToolResult: &p}
+	case BetaCodeExecutionToolResultBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfCodeExecutionToolResult: &p}
+	case BetaContainerUploadBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfContainerUpload: &p}
+	case BetaMCPToolResultBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfMCPToolResult: &p}
+	case BetaMCPToolUseBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfMCPToolUse: &p}
+	case BetaServerToolUseBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfServerToolUse: &p}
+	case BetaTextEditorCodeExecutionToolResultBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfTextEditorCodeExecutionToolResult: &p}
+	case BetaWebFetchToolResultBlock:
+		p := variant.ToParam()
+		return BetaContentBlockParamUnion{OfWebFetchToolResult: &p}
 	}
 	return BetaContentBlockParamUnion{}
 }
@@ -161,6 +189,23 @@ func (r BetaTextBlock) ToParam() BetaTextBlockParam {
 			citationParam.EndBlockIndex = citationVariant.EndBlockIndex
 			citationParam.StartBlockIndex = citationVariant.StartBlockIndex
 			p.Citations[i] = BetaTextCitationParamUnion{OfContentBlockLocation: &citationParam}
+		case BetaCitationSearchResultLocation:
+			var citationParam BetaCitationSearchResultLocationParam
+			citationParam.Type = citationVariant.Type
+			citationParam.CitedText = citationVariant.CitedText
+			citationParam.Title = paramutil.ToOpt(citationVariant.Title, citationVariant.JSON.Title)
+			citationParam.EndBlockIndex = citationVariant.EndBlockIndex
+			citationParam.StartBlockIndex = citationVariant.StartBlockIndex
+			citationParam.Source = citationVariant.Source
+			p.Citations[i] = BetaTextCitationParamUnion{OfSearchResultLocation: &citationParam}
+		case BetaCitationsWebSearchResultLocation:
+			var citationParam BetaCitationWebSearchResultLocationParam
+			citationParam.Type = citationVariant.Type
+			citationParam.CitedText = citationVariant.CitedText
+			citationParam.Title = paramutil.ToOpt(citationVariant.Title, citationVariant.JSON.Title)
+			p.Citations[i] = BetaTextCitationParamUnion{OfWebSearchResultLocation: &citationParam}
+		default:
+			panic(fmt.Sprintf("unexpected anthropic.anyBetaTextCitation: %#v", citationVariant))
 		}
 	}
 	return p
@@ -180,5 +225,137 @@ func (r BetaToolUseBlock) ToParam() BetaToolUseBlockParam {
 	p.ID = r.ID
 	p.Input = r.Input
 	p.Name = r.Name
+	return p
+}
+
+func (r BetaWebSearchResultBlock) ToParam() BetaWebSearchResultBlockParam {
+	var p BetaWebSearchResultBlockParam
+	p.Type = r.Type
+	p.EncryptedContent = r.EncryptedContent
+	p.Title = r.Title
+	p.URL = r.URL
+	p.PageAge = paramutil.ToOpt(r.PageAge, r.JSON.PageAge)
+	return p
+}
+
+func (r BetaWebSearchToolResultBlock) ToParam() BetaWebSearchToolResultBlockParam {
+	var p BetaWebSearchToolResultBlockParam
+	p.Type = r.Type
+	p.ToolUseID = r.ToolUseID
+
+	if len(r.Content.OfBetaWebSearchResultBlockArray) > 0 {
+		for _, block := range r.Content.OfBetaWebSearchResultBlockArray {
+			p.Content.OfResultBlock = append(p.Content.OfResultBlock, block.ToParam())
+		}
+	} else {
+		p.Content.OfError = &BetaWebSearchToolRequestErrorParam{
+			Type:      r.Content.Type,
+			ErrorCode: r.Content.ErrorCode,
+		}
+	}
+	return p
+}
+
+func (r BetaWebFetchToolResultBlock) ToParam() BetaWebFetchToolResultBlockParam {
+	var p BetaWebFetchToolResultBlockParam
+	p.Type = r.Type
+	p.ToolUseID = r.ToolUseID
+	return p
+}
+
+func (r BetaMCPToolUseBlock) ToParam() BetaMCPToolUseBlockParam {
+	var p BetaMCPToolUseBlockParam
+	p.Type = r.Type
+	p.ID = r.ID
+	p.Input = r.Input
+	p.Name = r.Name
+	p.ServerName = r.ServerName
+	return p
+}
+
+func (r BetaContainerUploadBlock) ToParam() BetaContainerUploadBlockParam {
+	var p BetaContainerUploadBlockParam
+	p.Type = r.Type
+	p.FileID = r.FileID
+	return p
+}
+
+func (r BetaServerToolUseBlock) ToParam() BetaServerToolUseBlockParam {
+	var p BetaServerToolUseBlockParam
+	p.Type = r.Type
+	p.ID = r.ID
+	p.Input = r.Input
+	p.Name = BetaServerToolUseBlockParamName(r.Name)
+	return p
+}
+
+func (r BetaTextEditorCodeExecutionToolResultBlock) ToParam() BetaTextEditorCodeExecutionToolResultBlockParam {
+	var p BetaTextEditorCodeExecutionToolResultBlockParam
+	p.Type = r.Type
+	p.ToolUseID = r.ToolUseID
+	p.Content = param.Override[BetaTextEditorCodeExecutionToolResultBlockParamContentUnion](r.Content.RawJSON())
+	return p
+}
+
+func (r BetaMCPToolResultBlock) ToParam() BetaRequestMCPToolResultBlockParam {
+	var p BetaRequestMCPToolResultBlockParam
+	p.Type = r.Type
+	p.ToolUseID = r.ToolUseID
+	if r.Content.JSON.OfString.Valid() {
+		p.Content.OfString = paramutil.ToOpt(r.Content.OfString, r.Content.JSON.OfString)
+	} else {
+		for _, block := range r.Content.OfBetaMCPToolResultBlockContent {
+			p.Content.OfBetaMCPToolResultBlockContent = append(p.Content.OfBetaMCPToolResultBlockContent, block.ToParam())
+		}
+	}
+	return p
+}
+
+func (r BetaBashCodeExecutionToolResultBlock) ToParam() BetaBashCodeExecutionToolResultBlockParam {
+	var p BetaBashCodeExecutionToolResultBlockParam
+	p.Type = r.Type
+	p.ToolUseID = r.ToolUseID
+
+	if r.Content.JSON.ErrorCode.Valid() {
+		p.Content.OfRequestBashCodeExecutionToolResultError = &BetaBashCodeExecutionToolResultErrorParam{
+			ErrorCode: BetaBashCodeExecutionToolResultErrorParamErrorCode(r.Content.ErrorCode),
+		}
+	} else {
+		requestBashContentResult := &BetaBashCodeExecutionResultBlockParam{
+			ReturnCode: r.Content.ReturnCode,
+			Stderr:     r.Content.Stderr,
+			Stdout:     r.Content.Stdout,
+		}
+
+		for _, block := range r.Content.Content {
+			requestBashContentResult.Content = append(requestBashContentResult.Content, block.ToParam())
+		}
+
+		p.Content.OfRequestBashCodeExecutionResultBlock = requestBashContentResult
+	}
+
+	r.Content.AsResponseBashCodeExecutionResultBlock()
+	p.Content = param.Override[BetaBashCodeExecutionToolResultBlockParamContentUnion](r.Content.RawJSON())
+	return p
+}
+
+func (r BetaBashCodeExecutionOutputBlock) ToParam() BetaBashCodeExecutionOutputBlockParam {
+	var p BetaBashCodeExecutionOutputBlockParam
+	p.Type = r.Type
+	p.FileID = r.FileID
+	return p
+}
+
+func (r BetaCodeExecutionToolResultBlock) ToParam() BetaCodeExecutionToolResultBlockParam {
+	var p BetaCodeExecutionToolResultBlockParam
+	p.Type = r.Type
+	p.ToolUseID = r.ToolUseID
+	p.Content = param.Override[BetaCodeExecutionToolResultBlockParamContentUnion](r.Content.RawJSON())
+	return p
+}
+func (r BetaCodeExecutionOutputBlock) ToParam() BetaCodeExecutionOutputBlockParam {
+	var p BetaCodeExecutionOutputBlockParam
+	p.Type = r.Type
+	p.FileID = r.FileID
 	return p
 }
